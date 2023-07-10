7a5399d7447dbc23e7817b0404693e25cfbf78e0
FERNANDA ESCARLETH NOYA MARQUEZ <fernanda.noya@inacapmail.cl>
2023-07-10 18:20:33 -0400

    Añadidas rutas y componentes

---
+++ src/components/Calama.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react"
+
+const Calama = () => {
+    return (
+        <>
+            <h1>Ciudad Calama</h1>
+            <p>Ciudad de Chile</p>
+        </>
+    )
+}
+
+export default Calama
---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,16 @@
+/* react core */
+import React, { useState, useEffect } from 'react'
+
+// function HolaMundo() {}
+/* View */
+const HolaMundo = () => {
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            {/* <p className='algo'>Diga algo</p> */}
+        </>
+    )
+}
+
+export default HolaMundo
